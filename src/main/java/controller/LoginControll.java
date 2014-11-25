fbfea0744d20afdcc70baac7ec45ddc596465bec
lpp <lipingping951462@163.com>
2014-11-25 09:53:31 +0800

    userlogin

--- src/main/java/controller/LoginControll.java
+++ src/main/java/controller/LoginControll.java
@@ -1,14 +1,21 @@
 package controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import util.MD5;
+import dao.AdmainDao;
 import forms.LoginForm;
 
 @Controller
 @RequestMapping("/user")
 public class LoginControll {
+	@Resource
+	private AdmainDao adminDao;
 
 //	@RequestMapping("/login")
 //	public String login(@RequestParam(value="name") String name,@RequestParam("password") String password, Object command){
@@ -23,8 +30,13 @@ public class LoginControll {
 		  }
 		  String name=loginForm.getName();
 		  String password=loginForm.getPassword();
-		  
-		return "index";
+		  String md5pass=MD5.GetMD5Code(password);
+		  //校验用户名密码是否正确
+		  boolean isuser=adminDao.validAdmin(name, md5pass);
+		  if(isuser){
+			  return "index";
+		  }
+		return "login";
 	}
 	
 }
